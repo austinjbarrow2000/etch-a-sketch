64cf686cf312baa6db66ce74c0f8fa6fb83370b9
Austin Barrow <austinjbarrow2000@gmail.com>
2022-10-02 16:07:53 -0400

    Position and style grid

--- script.js
+++ script.js
@@ -3,7 +3,15 @@ const container = document.querySelector('#container');
 for(let i = 0; i < 16**2; i++) {
     const div = document.createElement('div');
     div.classList.toggle('gridBox');
-    div.innerText = i;
     container.appendChild(div);
-    console.log(`Inserted square ${i}`);
+}
+
+const grids = document.querySelectorAll('.gridBox');
+
+grids.forEach( (grid) => grid.addEventListener('mouseover', function(e) {
+    hover(e)
+} ));
+
+function hover(e) {
+    e.target.classList.add('hover');
 }
