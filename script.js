dd1054d6528bc178d184374eee1d190a55ff83b2
Austin Barrow <austinjbarrow2000@gmail.com>
2022-10-02 16:24:36 -0400

    Add manipulation of grid size

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
 const container = document.querySelector('#container');
+size = 16;
+createGrid(16);
 
-for(let i = 0; i < 16**2; i++) {
-    const div = document.createElement('div');
-    div.classList.toggle('gridBox');
-    container.appendChild(div);
+function createGrid(size) {
+    container.innerHTML = '';
+    container.style["grid-template-columns"] = `repeat(${size}, 1fr)`;
+    for(let i = 0; i < size**2; i++) {
+        const div = document.createElement('div');
+        div.classList.toggle('gridBox');
+        container.appendChild(div);
+    }
+
+    const grids = document.querySelectorAll('.gridBox');
+
+    grids.forEach( (grid) => grid.addEventListener('mouseover', function(e) {
+        hover(e)
+    } ));
+
+    function hover(e) {
+        e.target.classList.add('hover');
+}
 }
 
-const grids = document.querySelectorAll('.gridBox');
+const sizeButton = document.querySelector('#sizeButton');
 
-grids.forEach( (grid) => grid.addEventListener('mouseover', function(e) {
-    hover(e)
-} ));
+sizeButton.addEventListener('click', changeSize);
 
-function hover(e) {
-    e.target.classList.add('hover');
+function changeSize() {
+    newSize = prompt("What size should the grid be?");
+    if(newSize > 100) {
+        alert('Did not change size. Keep 100 or below.');
+    } else {
+        createGrid(newSize);
+    }
 }
